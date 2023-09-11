a29f5d8ec460854b3708e38c2146005cdade37a8
Ujjwal Shaw <90507351+Ujjwalshaw@users.noreply.github.com>
2023-09-12 02:39:03 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -76,7 +76,7 @@ const Navbar = props => {
           </LinkItem>
           <LinkItem
             target="_blank"
-            href="https://github.com/Ujjwalshaw/Ujjwal-s-Portfolio"
+            href="https://github.com/Ujjwalshaw/Ujjwal-Portfolio"
             path={path}
             display="inline-flex"
             alignItems="center"
